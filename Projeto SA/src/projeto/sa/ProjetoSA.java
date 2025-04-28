691f75bcf5112ee6c0ae77c6374c7bd70d1c1a76
gustabernardo <gustagb00@gmail.com>
2025-04-28 10:35:59 -0300

    First commit

---
+++ Projeto SA/src/projeto/sa/ProjetoSA.java
@@ -0,0 +1,13 @@
+package projeto.sa;
+import java.util.Scanner;
+public class ProjetoSA {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+
+
+        sc.close();
+    }
+
+}
